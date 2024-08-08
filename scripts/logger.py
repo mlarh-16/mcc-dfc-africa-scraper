7ca8383641ffbdf8c947c8adad340bbf49d42aea
Jorge Rivera <jorge.rivera@one.org>
2024-08-08 15:08:19 +0200

    Update logger.py

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,25 +1,9 @@
 import logging
-from scripts.config import Paths
 
-# Create a root logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+    datefmt="%H:%M:%S",
+)
+
 logger = logging.getLogger(__name__)
-
-# Create the terminal handler
-shell_handler = logging.StreamHandler()
-
-# Set levels for the logger, shell and file
-logger.setLevel(logging.DEBUG)
-shell_handler.setLevel(logging.DEBUG)
-
-# Format the outputs
-fmt_file = "%(levelname)s (%(asctime)s): %(message)s"
-fmt_shell = "%(levelname)s [%(funcName)s:] %(message)s"
-
-# Create formatters
-shell_formatter = logging.Formatter(fmt_shell)
-
-# Add formatters to handlers
-shell_handler.setFormatter(shell_formatter)
-
-# Add handlers to the logger
-logger.addHandler(shell_handler)
