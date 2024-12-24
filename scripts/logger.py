ec805e99114e9701386c3bb4d696a3d5001c5c6c
Jorge Rivera <jorge.rivera@one.org>
2024-12-24 08:42:20 +0100

    Linting and pyproject changes

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,9 +1,30 @@
 import logging
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s [%(levelname)s]: %(message)s",
-    datefmt="%H:%M:%S",
-)
 
-logger = logging.getLogger(__name__)
+def setup_logger(name) -> logging.Logger:
+    """Set up the logger.
+
+    Args:
+        name (str): The name of the logger.
+
+    Returns:
+        logging.Logger: The logger.
+
+    """
+    logger_ = logging.getLogger(name)
+    logger_.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    console_handler.setFormatter(formatter)
+
+    logger_.addHandler(console_handler)
+
+    return logger_
+
+
+logger = setup_logger("research_project")
